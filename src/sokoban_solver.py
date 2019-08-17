db991a4cd2c181e4f97bab4d7453630d4f899147
jcapers <jcapers@users.noreply.github.com>
2019-08-17 17:55:42 +1000

    Created state class, sokoban solver setup, and goal testing.

--- src/sokoban_solver.py
+++ src/sokoban_solver.py
@@ -1,17 +1,77 @@
-from sokoban_map import SokobanMap
+from enum import Enum
 from typing import List
+import heapq
 import sys
 
+# Internal package imports
+from state import State
+from sokoban_map import SokobanMap
 
-def main(args: List[str]):
-    input_file = args[0]
-    sokoban = SokobanMap(input_file)
-    print(f'Obstacle Map\n{sokoban.obstacle_map}')
-    print(f'Player Position\n{sokoban.player_position}')
-    print(f'Target Positions\n{sokoban.tgt_positions}')
-    print(f'Box Positions\n{sokoban.box_positions}')
+"""
+COMP3702 Assignment 1: Sokoban Solver
+
+References
+[1]
+[2]
+"""
+
+
+class SearchStrategy(Enum):
+    UCS = 1
+    AStar = 2
+
+
+class SokobanSolver:
+    """Sokoban Solver, utilises Uniform Cost Search OR A* Search."""
+
+    def __init__(self, input_filename: str, output_filename: str):
+        self.map_file = input_filename
+        self.output_file = output_filename
+
+        # Load the initial state of a Sokoban game, keep track of target and obstacles in sokoban_map
+        self.sokoban_map = SokobanMap(self.map_file)
+
+        # Setup for search
+        self.initial_state = State(None, self.sokoban_map.box_positions[:], self.sokoban_map.player_position)
+        self.frontier = [self.initial_state]
+        self.visited = []
+
+    def search(self, strategy: SearchStrategy):
+        """
+        Search for solution to Sokoban game.
+
+        If strategy is A* then will incorporate heuristic function.
+
+        Algorithm modified but based on pseudocode provided by Russsell and Norvig [1]
+        and Alina Bialkowski's lecture notes [2].
+
+        :param strategy:
+        :return:
+        """
+        pass
+
+    def goal_test(self, state):
+        """
+        Test if state is at goal by comparing box positions with target positions.
+
+        :param state: the state to check if the goal has been reached.
+        :return: True if state is the goal state, else False.
+        """
+        for box in state.box_positions:
+            if box not in self.sokoban_map.tgt_positions:
+                return False
+        return True
+
+
+def main(arglist: List[str]):
+    if len(arglist) != 2:
+        print("Sokoban Solver needs an input file and an output file name.")
+        print("Usage: sokoban_solver.py [map_file_name] [solution_file_name]")
+        return
+    input_file = arglist[0]
+    output_file = arglist[1]
+    sokoban_solver = SokobanSolver(input_file, output_file)
 
 
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    main(args)
+    main(sys.argv[1:])
---
+++ src/state.py
@@ -0,0 +1,39 @@
+from typing import List, Tuple
+
+
+class State:
+    """A node that holds a Sokoban State."""
+
+    def __init__(self,
+                 parent,
+                 box_positions: List[Tuple[int, int]],
+                 player_position: Tuple[int, int],
+                 move: str = None):
+        self.parent = parent
+        self.box_positions = box_positions
+        self.player_position = player_position
+        # The move taken to get to this state.
+        self.move_taken = move
+        # g(n) cost to get here. Cost is 0 if this node is the root node, with parent == None.
+        if self.parent is None:
+            self.cost = 0
+        else:
+            self.cost = self.parent.cost + self.distance_to_parent()
+
+    def distance_to_parent(self):
+        """
+        Computes the distance from this state, to another state.
+
+        Distance increments at least 1 since the player always moves on each step.
+        If a box has been successfully moved, then the box has moved by a distance of 1 as well.
+
+        For the purposes of the Sokoban Solver, distance g(n) is only relevant to the immediate parent.
+
+        :return: Distance cost from parent to this State.
+        """
+        distance = 1
+        # Get box cost, if a box has moved from last state then increment cost.
+        for pos in self.parent.box_positions:
+            if pos not in self.box_positions:
+                distance += 1
+        return distance
